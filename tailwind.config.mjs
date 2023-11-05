a6cc3eee00416e57da0bcbed4a6067edbe75fff6
Glendale Acosta <gglendale17@gmail.com>
2023-11-05 01:08:15 -0800

    make the damn website

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			// fontFamily: {
+			// sans: ["Geist", "Geist-sans"],
+			// sans: ['var(--font-geist-sans)'],
+			// 	mono: ['var(--font-geist-mono)'],
+			// },
+			colors: {
+				'ass-black': '#141517',
+				'ass-black-400': '#23262d',
+				'ass-rgba-10p': 'rgba(255, 255, 255, 0.1)'
+			}
+		},
 	},
 	plugins: [],
 }
