c14adc083e23e94bbd75bc25ee3815c7a9423a22
Glendale Acosta <gglendale17@gmail.com>
2023-11-05 01:27:31 -0800

    add ssl

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
+import basicSsl from '@vitejs/plugin-basic-ssl'
 import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  vite: {
+    plugins: [basicSsl()]
+  }
 });
