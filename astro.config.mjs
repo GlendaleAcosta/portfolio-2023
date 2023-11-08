bf4a1f664f46e30ccf15e34726d61b31adb30993
Glendale Acosta <gglendale17@gmail.com>
2023-11-08 15:50:45 -0800

    add vercel web analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,18 @@
-import basicSsl from '@vitejs/plugin-basic-ssl'
+import basicSsl from '@vitejs/plugin-basic-ssl';
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   vite: {
     plugins: [basicSsl()]
-  }
+  },
+  output: "server",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  })
 });
